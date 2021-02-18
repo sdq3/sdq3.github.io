d1c035a4ccbae14d938a71bcdd96a641f6bfb422
sdqOS <2088750435@qq.com>
2021-02-18 21:40:43 +0800

    Site updated: 2021-02-18 21:40:42

--- js/welcome.js
+++ js/welcome.js
@@ -6,19 +6,6 @@ Function.prototype.getMultiLine = function () {
 		return lines;
 }
 var str = function () {
-/*
-=====================================================================
-     _                                         _               
-    | | It gives people a sense of emptiness  | |              
-  __| |  ___   __ _  _ __  _ __ ___   ___   __| |  __ _  _ __  
- / _` | / _ \ / _` || '__|| '_ ` _ \ / __| / _` | / _` || '_ \ 
-| (_| ||  __/| (_| || |   | | | | | |\__ \| (_| || (_| || | | |
- \__,_| \___| \__,_||_|   |_| |_| |_||___/ \__,_| \__,_||_| |_|
-                                                               
- :) welcoming you to...
- hexo-theme-dearmsdan (https://github.com/ZHD99/hexo-theme-dearmsdan)
-                       
-=====================================================================
-*/
+
 }
 logger.info(str.getMultiLine());
